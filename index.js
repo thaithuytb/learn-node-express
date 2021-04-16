616d8105645d10d3d3ab2baa2ac45772347a53d3
thaithuytb <ngoquangthai29112001@gmail.com>
2021-04-16 16:55:08 +0700

    learn about: morgan

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
-
+app.use(morgan('combined'))
 const PORT = 3999;
 
 app.get('/', (req, res) => res.send(`<h1>Hello woul ddd</h2>`));
